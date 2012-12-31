b32a4d0098a85dade23168dce05494492d905946
szqh97 <szqh97@163.com>
2012-12-31 22:09:58 +0800

    init the curl_handle

--- getinmemery.c
+++ getinmemery.c
@@ -45,6 +45,7 @@ static size_t write2MemoryCallback(void *contents, size_t size, size_t nmemb, vo
 
 int main ( int argc, char *argv[] )
 {
+    //const char *url = "http://www.baidu.com";
     const char *url = "http://ui.ptlogin2.qq.com/cgi-bin/ver";
     CURL *curl_handle;
     
@@ -53,17 +54,18 @@ int main ( int argc, char *argv[] )
     chunk.size = 0;
     
     curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
 
     curl_easy_setopt(curl_handle, CURLOPT_URL, url);
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, &write2MemoryCallback);
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-    //curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
     curl_easy_perform(curl_handle);
     curl_easy_cleanup(curl_handle);
 
     printf("%lu bytes retrieved\n", (long)chunk.size);
-    printf("\n---------\n %s \n--------\n", chunk.memory);
+    printf("\n --------\n %s ---------\n", chunk.memory);
     
     if (chunk.memory)
         free(chunk.memory);
